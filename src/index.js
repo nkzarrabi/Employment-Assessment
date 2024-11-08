d700b2ed32ce82f8c20f17868b7260e43b1e411a
Nick Z <48893251+nkzarrabi@users.noreply.github.com>
2024-11-08 16:16:08 -0500

    Add React entry point for HRInterface component
    * Import React and ReactDOM libraries
    * Import HRInterface component
    * Render HRInterface component inside a div with id `hr-interface-root` using ReactDOM.render

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HRInterface from './components/HRInterface';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HRInterface />
+  </React.StrictMode>,
+  document.getElementById('hr-interface-root')
+);
