84ee0ee1aae7ca8a850bfeb0ed8a8d7163c05771
Nick Z <48893251+nkzarrabi@users.noreply.github.com>
2024-11-08 16:21:54 -0500

    Add React integration to HR Dashboard
    * **hr_dashboard/templates/hr_dashboard/index.html**
      - Import `HRInterface` component from `src/components/HRInterface.js`
      - Add `HRInterface` component to the body of the HTML
      - Move script tag to the head section
    
    * **package.json**
      - Create `package.json` file with necessary dependencies for React
      - Add scripts for start, build, test, and eject
    
    * **src/App.js**
      - Create basic React App component with `HRInterface`
    
    * **src/App.css**
      - Add basic CSS for React App
    
    * **package-lock.json**
      - Add package-lock.json with React dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HRInterface from './components/HRInterface';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Employment Assessment</h1>
+      </header>
+      <HRInterface />
+    </div>
+  );
+}
+
+export default App;
