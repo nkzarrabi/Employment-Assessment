21f358b98341badd58419e983fb9e432f50de5ed
Nick Z <48893251+nkzarrabi@users.noreply.github.com>
2024-11-09 09:56:37 -0500

    Fix HR_Interface to point to React index HTML
    Fixes #36
    
    Update HR interface to point to React index HTML.
    
    * Modify `EmploymentAssessment/settings.py` to add `BASE_DIR / 'react_app/build'` to the `TEMPLATES` `DIRS` list.
    * Modify `EmploymentAssessment/urls.py` to add a new path for the React app and import `TemplateView` from `django.views.generic`.
    * Modify `hr_dashboard/views.py` to update the `hr_interface` view to render the React index HTML.
    * Delete `hr_dashboard/templates/hr_dashboard/index.html`.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/nkzarrabi/Employment-Assessment/issues/36?shareId=XXXX-XXXX-XXXX-XXXX).

--- EmploymentAssessment/settings.py
+++ EmploymentAssessment/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'EmploymentAssessment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'react_app/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- EmploymentAssessment/urls.py
+++ EmploymentAssessment/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hr_dashboard/', include('hr_dashboard.urls')),
     path('role_customization/', include('role_customization.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='react_app'),
 ]
